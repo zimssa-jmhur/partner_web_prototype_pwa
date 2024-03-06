ab7e52032c40a413c4901230ffec9103ecc1eaec
허정민 <jmhur@zimssa.com>
2024-03-06 11:17:01 +0900

    feat: fcm 토큰 세팅

---
+++ src/app/hooks/useFcmToken.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { getMessaging, getToken } from 'firebase/messaging';
+import firebaseApp from '../utils/firebase/firebase';
+
+const useFcmToken = () => {
+  const [token, setToken] = useState('');
+  const [notificationPermissionStatus, setNotificationPermissionStatus] =
+    useState('');
+
+  useEffect(() => {
+    const retrieveToken = async () => {
+      try {
+        if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+          const messaging = getMessaging(firebaseApp);
+          // Retrieve the notification permission status
+          console.log('are you asking permission?');
+          const permission = await Notification.requestPermission();
+          setNotificationPermissionStatus(permission);
+          console.log(permission)
+          // Check if permission is granted before retrieving the token
+          if (permission === 'granted') {
+            const currentToken = await getToken(messaging, {
+              vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
+            });
+            if (currentToken) {
+              setToken(currentToken);
+            } else {
+              console.log(
+                'No registration token available. Request permission to generate one.'
+              );
+            }
+          }
+        }
+      } catch (error) {
+        console.log('An error occurred while retrieving token:', error);
+      }
+    };
+
+    retrieveToken();
+  }, []);
+
+  return { fcmToken: token, notificationPermissionStatus };
+};
+
+export default useFcmToken;
